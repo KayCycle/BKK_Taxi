636e3da41f737037536b1afbeea06ec95feae86d
Kaiden <105527984+KayCycle@users.noreply.github.com>
2024-04-03 19:31:45 +0200

    Update short_trip_plotting.py

--- short_trip_plotting.py
+++ short_trip_plotting.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# Define the path to the CSV file
+# csv file path
 CSV_FILE_PATH = '22data/jan22data/jan22_sorted_vacant_trips.csv'
-# Define the time category of interest
+# time category to plot
 TIME_CATEGORY = '0-3'
 # save path
 save_dir = "22data/jan22data"  # save directory
-EPSILON = 0.1  # Consider this as the radius of neighborhood in kilometers, adjust as necessary
-MIN_SAMPLES = 25 # Minimum samples in a neighborhood for a point to be considered as a core point
+EPSILON = 0.1  # radius of "neighborhood" in kilometers
+MIN_SAMPLES = 25 # min samples in a neighborhood for a point to be considered as a core point (to be plotted)
 
 def read_data(file_path):
     return pd.read_csv(file_path)
@@ -20,22 +20,22 @@ def filter_data_by_time_category(data, time_category):
     return data[data['time_category'] == time_category]
 
 def apply_dbscan(data, eps, min_samples):
-    # Convert to radians for sklearn Haversine metric
+    # conversion
     coords = np.radians(data[['pickup.lat', 'pickup.lon']])
     db = DBSCAN(eps=eps/6371, min_samples=min_samples, algorithm='ball_tree', metric='haversine').fit(coords)
     data['cluster'] = db.labels_
     return data
 
 def filter_clusters_with_minimum_trips(data, min_trips):
-    # Count the number of trips in each cluster
+    # count num trips in each cluster
     cluster_counts = data['cluster'].value_counts()
-    # Filter clusters having trips >= MIN_TRIPS and exclude noise (cluster = -1)
+    # filter clusters with more than min samples and noise goes away
     valid_clusters = cluster_counts[(cluster_counts >= min_trips) & (cluster_counts.index != -1)].index
     return data[data['cluster'].isin(valid_clusters)]
 
 def plot_clusters(data):
     plt.figure(figsize=(12, 8))
-    # Plot each cluster with a different color
+    # plot with different color
     clusters = data['cluster'].unique()
     for cluster in clusters:
         cluster_data = data[data['cluster'] == cluster]
